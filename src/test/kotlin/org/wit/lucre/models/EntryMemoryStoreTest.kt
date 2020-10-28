54386b815afa756d84397e65988041931e6c3882
Gianluca <gianlucazuccarelli@gmail.com>
2020-10-28 19:23:24 +0000

    chore: add random id dep

---
+++ src/test/kotlin/org/wit/lucre/models/EntryMemoryStoreTest.kt
@@ -0,0 +1,24 @@
+package org.wit.lucre.models
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class EntryMemoryStoreTest {
+
+    @Test
+    fun all() {
+    }
+
+    @Test
+    fun find() {
+    }
+
+    @Test
+    fun create() {
+    }
+
+    @Test
+    fun update() {
+    }
+}
