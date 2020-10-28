b0de9863e934d8d0bab7248730c0786d9709f8f0
Gianluca <gianlucazuccarelli@gmail.com>
2020-10-28 23:34:01 +0000

    feat: add income service

---
+++ src/main/kotlin/org/wit/lucre/services/IncomeService.kt
@@ -0,0 +1,20 @@
+package org.wit.lucre.services
+
+import org.wit.lucre.models.EntryModel
+import org.wit.lucre.models.EntryType
+
+class IncomeService() {
+
+    fun balance(entries: List<EntryModel>): Int {
+
+        val values = entries.map{
+            if (it.type.equals(EntryType.INCOME))
+                it.amount
+            else it.amount * -1
+        }
+
+        return values.reduce{acc, it -> acc + it}
+
+    }
+
+}
---
+++ src/test/kotlin/org/wit/lucre/services/IncomeServiceTest.kt
@@ -0,0 +1,12 @@
+package org.wit.lucre.services
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class IncomeServiceTest {
+
+    @Test
+    fun balance() {
+    }
+}
