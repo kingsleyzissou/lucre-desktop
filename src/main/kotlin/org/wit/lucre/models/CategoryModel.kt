ce872b09a83eed9303e550e9a54edbb7f9f07603
Gianluca <gianlucazuccarelli@gmail.com>
2020-10-30 13:34:25 +0000

    chore: refactor category model

--- src/main/kotlin/org/wit/lucre/models/CategoryModel.kt
+++ src/main/kotlin/org/wit/lucre/models/CategoryModel.kt
@@ -1,7 +1,10 @@
 package org.wit.lucre.models
 
+import com.aventrix.jnanoid.jnanoid.NanoIdUtils
+
 class CategoryModel(
     var name: String,
     var description: String,
-    var color: String
+    var color: String,
+    var id: String = NanoIdUtils.randomNanoId()
 )
