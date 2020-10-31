0316a5fbdf52a53886e5b61680fcff9f4d73f0a4
Gianluca <gianlucazuccarelli@gmail.com>
2020-10-31 21:24:06 +0000

    feat: add basic create view

--- src/main/kotlin/org/wit/lucre/views/vault/VaultCreate.kt
+++ src/main/kotlin/org/wit/lucre/views/vault/VaultCreate.kt
@@ -1,9 +1,58 @@
 package org.wit.lucre.views.vault
 
+import javafx.collections.FXCollections
+import javafx.scene.control.ComboBox
+import javafx.scene.control.TextField
 import tornadofx.Fragment
-import tornadofx.borderpane
+import tornadofx.action
+import tornadofx.button
+import tornadofx.label
+import tornadofx.textfield
+import tornadofx.hbox
+import tornadofx.singleAssign
+import tornadofx.vbox
+import tornadofx.combobox
 
 class VaultCreate : Fragment("Create Vault") {
-    override val root = borderpane {
+
+    private var nameField: TextField by singleAssign()
+    private var descriptionField: TextField by singleAssign()
+    private var currencyField: ComboBox<String> by singleAssign()
+
+    override val root = vbox {
+        hbox {
+            label("Vault name: ")
+            nameField = textfield()
+        }
+        hbox {
+            label("Description: ")
+            descriptionField = textfield()
+        }
+        hbox {
+            label("Currency: ")
+            currencyField = combobox<String>() {
+                items = FXCollections.observableArrayList(
+                    "$", "£", "€", "AED", "R", "R$", "¥"
+                )
+            }
+        }
+        hbox {
+            button("Clear") {
+                action { clear() }
+            }
+            button("Create") {
+                action { create() }
+            }
+        }
+    }
+
+    private fun clear() {
+        nameField.clear()
+        descriptionField.clear()
+        currencyField.selectionModel.clearSelection()
+    }
+
+    private fun create() {
+
     }
 }
