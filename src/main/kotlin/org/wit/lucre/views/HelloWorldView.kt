7edf37d360705d6818339b8a7c138ae681ee3787
Gianluca <gianlucazuccarelli@gmail.com>
2020-10-31 12:50:32 +0000

    test: check cicd pipeline

--- src/main/kotlin/org/wit/lucre/views/HelloWorldView.kt
+++ src/main/kotlin/org/wit/lucre/views/HelloWorldView.kt
@@ -1,19 +1,15 @@
 package org.wit.lucre.views
 
-import javafx.scene.paint.Color
-import tornadofx.View
-import tornadofx.borderpane
-import tornadofx.label
-import tornadofx.style
-import tornadofx.useMaxWidth
+import tornadofx.*
 
-class HelloWorldView : View("Hello there") {
-    override val root = borderpane {
-        top = label("TOP") {
-            useMaxWidth = true
-            style {
-                backgroundColor += Color.RED
-            }
-        }
+class HelloWorldView() : View("Hello there") {
+//    override val root = vbox {
+//        button("Press me")
+//        label("Waiting")
+//    }
+    val data = listOf<String>("One", "Two", "Three")
+
+    override val root = datagrid(data) {
+//        label(it)
     }
 }
