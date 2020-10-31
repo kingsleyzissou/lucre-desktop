cd6e51c8fe33c0a74853723f54ea009b8864003d
Gianluca <gianlucazuccarelli@gmail.com>
2020-10-31 22:56:22 +0000

    chore: refactor create vault to use view model

--- src/main/kotlin/org/wit/lucre/views/vault/VaultCreate.kt
+++ src/main/kotlin/org/wit/lucre/views/vault/VaultCreate.kt
@@ -1,58 +1,65 @@
 package org.wit.lucre.views.vault
 
+import javafx.beans.property.SimpleStringProperty
 import javafx.collections.FXCollections
 import javafx.scene.control.ComboBox
 import javafx.scene.control.TextField
-import tornadofx.Fragment
-import tornadofx.action
-import tornadofx.button
-import tornadofx.label
-import tornadofx.textfield
-import tornadofx.hbox
-import tornadofx.singleAssign
-import tornadofx.vbox
-import tornadofx.combobox
+import org.wit.lucre.controllers.VaultController
+import org.wit.lucre.models.Vault
+import org.wit.lucre.viewmodels.VaultModel
+import tornadofx.* // ktlint-disable no-wildcard-imports
 
 class VaultCreate : Fragment("Create Vault") {
 
-    private var nameField: TextField by singleAssign()
-    private var descriptionField: TextField by singleAssign()
-    private var currencyField: ComboBox<String> by singleAssign()
+    private val vaultController: VaultController by inject()
+    private var model = VaultModel(Vault())
+
+    private val name = model.bind{ SimpleStringProperty() }
+    private val description = model.bind { SimpleStringProperty() }
+    private  val currency = model.bind{ SimpleStringProperty() }
+
+    private val currencies = listOf<String>(
+            "$", "£", "€", "AED", "R", "R$", "¥"
+    )
 
     override val root = vbox {
-        hbox {
-            label("Vault name: ")
-            nameField = textfield()
-        }
-        hbox {
-            label("Description: ")
-            descriptionField = textfield()
-        }
-        hbox {
-            label("Currency: ")
-            currencyField = combobox<String>() {
-                items = FXCollections.observableArrayList(
-                    "$", "£", "€", "AED", "R", "R$", "¥"
-                )
+        form {
+            fieldset {
+                field("Vault name:") {
+                    textfield(name).required()
+                }
+                field("Description:") {
+                    textfield(description)
+                }
+                field("Currency:") {
+                    combobox<String>(currency, currencies).required()
+                    useMaxSize = true
+                }
+                field {
+                    button("Clear") {
+                        action { model.rollback() }
+                        isCancelButton = true
+                    }
+                    button("Create") {
+                        enableWhen(model.valid)
+                        isDefaultButton = true
+                        action {
+                            model.commit()
+                            create()
+                        }
+                    }
+                }
             }
         }
-        hbox {
-            button("Clear") {
-                action { clear() }
-            }
-            button("Create") {
-                action { create() }
-            }
-        }
-    }
-
-    private fun clear() {
-        nameField.clear()
-        descriptionField.clear()
-        currencyField.selectionModel.clearSelection()
     }
 
     private fun create() {
-
+        vaultController.create(
+            name.value,
+            currency.value,
+            description.value
+        )
+        replaceWith(VaultIndex::class)
     }
+
 }
