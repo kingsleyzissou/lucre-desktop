3c71fa76d1b263765c769567372cac79223614fd
Gianluca <gianlucazuccarelli@gmail.com>
2020-11-01 17:56:45 +0000

    feat: add serialize & deserialize methods to vault store

--- src/main/kotlin/org/wit/lucre/repositories/VaultStore.kt
+++ src/main/kotlin/org/wit/lucre/repositories/VaultStore.kt
@@ -1,29 +1,19 @@
 package org.wit.lucre.repositories
 
-import org.wit.lucre.models.JsonList
 import org.wit.lucre.models.Vault
 import org.wit.lucre.utilities.read
-import tornadofx.JsonModel
-import tornadofx.save
-import java.io.FileOutputStream
-import java.io.StringReader
+import org.wit.lucre.utilities.write
+import tornadofx.jsonArray
 import javax.json.Json
 import javax.json.JsonObject
-import javax.json.JsonReader
 
-class VaultStore : CRUDStore<Vault>("vaults.json") {
-
-    override fun all(): List<Vault> {
-        return list.values.toList()
-    }
-
-    override fun find(id: String): Vault? {
-        return list[id]
-    }
+class VaultStore : CRUDStore<Vault>(
+    "vaults.json"
+) {
 
     override fun create(value: Vault) {
         list[value.id] = value
-        serialize(value)
+        serialize()
     }
 
     override fun update(value: Vault) {
@@ -32,31 +22,25 @@ class VaultStore : CRUDStore<Vault>("vaults.json") {
             vault.name = value.name
             vault.description = value.description
             vault.currency = value.currency
+            serialize()
         }
-        serialize(value)
     }
 
-    override fun addAll(values: List<Vault>) {
-        values.forEach { v -> this.create(v) }
-    }
-
-    override fun serialize(value: Vault) {
-        val output = FileOutputStream("vaults.json")
-        val arrList: List<JsonModel> = list.values.map { vault: Vault -> vault }
-        val jsonList = JsonList(arrList)
-        jsonList.save(output)
+    override fun serialize() {
+        var jsonObject = Json.createObjectBuilder()
+        var jsonArray = Json.createArrayBuilder()
+        all().map { jsonArray.add(it.toJSON()) }
+        jsonObject.add("list", jsonArray)
+        write(filename, jsonObject.build().toString())
     }
 
     override fun deserialize() {
-        val file = read(filename)
-        val jsonReader: JsonReader = Json.createReader(StringReader(file))
-        val jsonArr: JsonObject = jsonReader.readObject()
-        jsonReader.close()
-        val jsonList = JsonList(ArrayList())
-        jsonList.updateModel(jsonArr)
-        for (it: JsonModel in jsonList.list) {
-            val vault: Vault = it as Vault
-            list.put(vault.id, vault)
+        var contents: JsonObject = read(filename)!!
+        var arr = contents.jsonArray("list")!!
+        arr.forEach {
+            val vault = Vault()
+            vault.updateModel(it as JsonObject)
+            list[vault.id] = vault
         }
     }
 }
