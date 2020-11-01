560cafae1a2badd165ee47265f85649e28c3e453
Gianluca <gianlucazuccarelli@gmail.com>
2020-11-01 17:56:01 +0000

    chore: move common crudstore methods to abstract class

--- src/main/kotlin/org/wit/lucre/repositories/CRUDStore.kt
+++ src/main/kotlin/org/wit/lucre/repositories/CRUDStore.kt
@@ -3,10 +3,10 @@ package org.wit.lucre.repositories
 import mu.KotlinLogging
 import org.wit.lucre.utilities.fileExists
 
-abstract class CRUDStore<JsonModel>(var filename: String) : CRUDRepositoryInterface<JsonModel> {
+abstract class CRUDStore<T>(var filename: String) : CRUDRepositoryInterface<T> {
 
     private val logger = KotlinLogging.logger {}
-    var list: HashMap<String, JsonModel> = HashMap()
+    var list: HashMap<String, T> = HashMap()
 
     init {
         if (fileExists(filename)) {
@@ -14,11 +14,23 @@ abstract class CRUDStore<JsonModel>(var filename: String) : CRUDRepositoryInterf
         }
     }
 
-    abstract fun serialize(value: JsonModel)
+    override fun all(): List<T> {
+        return list.values.toList()
+    }
 
-    abstract fun deserialize()
+    override fun find(id: String): T? {
+        return list[id]
+    }
+
+    override fun addAll(values: List<T>) {
+        values.forEach { v -> this.create(v) }
+    }
 
     internal fun logAll() {
         list.forEach { logger.info("$it") }
     }
+
+    abstract fun serialize()
+
+    abstract fun deserialize()
 }
