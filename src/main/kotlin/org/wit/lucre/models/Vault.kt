8e1717760a59a85ae9fb7d7eba2899c7a2888e5e
Gianluca <gianlucazuccarelli@gmail.com>
2020-11-01 15:04:33 +0000

    feat: add json serialization to vault model

--- src/main/kotlin/org/wit/lucre/models/Vault.kt
+++ src/main/kotlin/org/wit/lucre/models/Vault.kt
@@ -3,13 +3,14 @@ package org.wit.lucre.models
 import com.aventrix.jnanoid.jnanoid.NanoIdUtils
 import javafx.beans.property.SimpleStringProperty
 import tornadofx.* // ktlint-disable no-wildcard-imports
+import javax.json.JsonObject
 
 class Vault(
     name: String? = null,
     description: String? = null,
     currency: String? = null,
     var id: String = NanoIdUtils.randomNanoId()
-) {
+) : JsonModel {
     val nameProperty = SimpleStringProperty(this, "name", name)
     val descProperty = SimpleStringProperty(this, "description", description)
     val currencyProperty = SimpleStringProperty(this, "currency", currency)
@@ -17,4 +18,22 @@ class Vault(
     var name: String by nameProperty
     var description: String by descProperty
     var currency: String by currencyProperty
+
+    override fun updateModel(json: JsonObject) {
+        with(json) {
+            name = string("name").toString()
+            description = string("description").toString()
+            currency = string("currency").toString()
+            id = string("id").toString()
+        }
+    }
+
+    override fun toJSON(json: JsonBuilder) {
+        with(json) {
+            add("name", name)
+            add("description", description)
+            add("currency", currency)
+            add("id", id)
+        }
+    }
 }
