6d57723192f61506df6587f6511d9413804fe6b3
Gianluca <gianlucazuccarelli@gmail.com>
2020-11-02 23:55:24 +0000

    feat: entry index view

---
+++ src/main/kotlin/org/wit/lucre/views/entry/EntryIndex.kt
@@ -0,0 +1,23 @@
+package org.wit.lucre.views.entry
+
+import org.wit.lucre.controllers.EntryController
+import org.wit.lucre.controllers.VaultController
+import org.wit.lucre.models.Entry
+import org.wit.lucre.viewmodels.EntryModel
+import org.wit.lucre.viewmodels.VaultModel
+import tornadofx.*
+
+class EntryIndex : Fragment("List Entries") {
+    val vault: VaultModel by inject()
+    val model = EntryModel(Entry())
+    private val entryController: EntryController by inject()
+    private val data = entryController.index().asObservable()
+
+    override val root = tableview(data) {
+        readonlyColumn("Vendor", Entry::vendor)
+        readonlyColumn("Description", Entry::description)
+        readonlyColumn("Category", Entry::category)
+        readonlyColumn("Amount (${vault.item.currency})", Entry::amount)
+        bindSelected(model)
+    }
+}
