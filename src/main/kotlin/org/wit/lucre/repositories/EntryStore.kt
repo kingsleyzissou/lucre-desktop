284042187479d8da57210477c5cd23e3d1a2e0f2
Gianluca <gianlucazuccarelli@gmail.com>
2020-11-02 20:17:58 +0000

    feat: implement entry store update and deserialize

--- src/main/kotlin/org/wit/lucre/repositories/EntryStore.kt
+++ src/main/kotlin/org/wit/lucre/repositories/EntryStore.kt
@@ -1,28 +1,30 @@
 package org.wit.lucre.repositories
 
 import org.wit.lucre.models.Entry
+import org.wit.lucre.utilities.read
+import tornadofx.jsonArray
+import tornadofx.toModel
+import javax.json.JsonObject
 
-class EntryStore : CRUDRepositoryInterface<Entry> {
-
-    private val entries = HashMap<String, Entry>()
-
-    override fun all(): List<Entry> {
-        return entries.values.toList()
-    }
-
-    override fun find(id: String): Entry? {
-        return entries[id]
-    }
-
-    override fun create(value: Entry) {
-        entries[value.id] = value
-    }
+class EntryStore : CRUDStore<Entry>("entries.json") {
 
     override fun update(value: Entry) {
-        entries[value.id] = value
+        var entry = find(value.id)
+        if (entry != null) {
+            entry.amount = value.amount
+            entry.type = value.type
+            entry.vendor = value.vendor
+            entry.description = value.description
+            entry.category = value.category
+            entry.vault = value.vault
+            entry.date = value.date
+            serialize()
+        }
     }
 
-    override fun addAll(values: List<Entry>) {
-        values.forEach { e -> this.create(e) }
+    override fun deserialize() {
+        var contents: JsonObject = read(filename)!!
+        var arr = contents.jsonArray("list")?.toModel<Entry>()
+        arr?.forEach { list[it.id] = it }
     }
 }
