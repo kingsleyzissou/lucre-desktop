4544abb88642142516eb23ecf1cabab2d196016b
Gianluca <gianlucazuccarelli@gmail.com>
2020-11-02 21:51:22 +0000

    feat: create Category ViewModel

--- src/main/kotlin/org/wit/lucre/models/Category.kt
+++ src/main/kotlin/org/wit/lucre/models/Category.kt
@@ -1,10 +1,21 @@
 package org.wit.lucre.models
 
 import com.aventrix.jnanoid.jnanoid.NanoIdUtils
+import tornadofx.getProperty
+import tornadofx.property
 
 class Category(
-    var name: String,
-    var description: String,
-    var color: String,
+    name: String,
+    description: String,
+    color: String,
     var id: String = NanoIdUtils.randomNanoId()
-)
+) {
+    fun nameProperty() = getProperty(Category::name)
+    fun descProperty() = getProperty(Category::description)
+    fun colorProperty() = getProperty(Category::color)
+
+    var name: String by property(name)
+    var description: String by property(description)
+    var color: String by property(color)
+
+}
