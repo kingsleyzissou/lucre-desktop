5a1d24be16025ff97637128ee3dc1f151a5df246
Gianluca <gianlucazuccarelli@gmail.com>
2020-11-02 21:52:50 +0000

    feat: Category implement JsonModel

--- src/main/kotlin/org/wit/lucre/models/Category.kt
+++ src/main/kotlin/org/wit/lucre/models/Category.kt
@@ -1,15 +1,18 @@
 package org.wit.lucre.models
 
 import com.aventrix.jnanoid.jnanoid.NanoIdUtils
+import tornadofx.JsonBuilder
 import tornadofx.getProperty
 import tornadofx.property
+import tornadofx.string
+import javax.json.JsonObject
 
 class Category(
     name: String,
     description: String,
     color: String,
-    var id: String = NanoIdUtils.randomNanoId()
-) {
+    override var id: String = NanoIdUtils.randomNanoId()
+): Model() {
     fun nameProperty() = getProperty(Category::name)
     fun descProperty() = getProperty(Category::description)
     fun colorProperty() = getProperty(Category::color)
@@ -18,4 +21,21 @@ class Category(
     var description: String by property(description)
     var color: String by property(color)
 
+    override fun updateModel(json: JsonObject) {
+        with(json) {
+            name = string("name").toString()
+            description = string("description").toString()
+            color = string("color").toString()
+            id = string("id").toString()
+        }
+    }
+
+    override fun toJSON(json: JsonBuilder) {
+        with(json) {
+            add("name", name)
+            add("description", description)
+            add("color", color)
+            add("id", id)
+        }
+    }
 }
