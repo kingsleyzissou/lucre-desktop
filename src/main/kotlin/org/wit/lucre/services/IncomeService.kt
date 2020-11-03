ed1c25da75c7bd4af8f24c82afbe4706e6048b37
Gianluca <gianlucazuccarelli@gmail.com>
2020-11-03 19:55:22 +0000

    refactor: income service balance calculation

--- src/main/kotlin/org/wit/lucre/services/IncomeService.kt
+++ src/main/kotlin/org/wit/lucre/services/IncomeService.kt
@@ -1,18 +1,12 @@
 package org.wit.lucre.services
 
 import org.wit.lucre.models.Entry
-import org.wit.lucre.models.EntryType
 
 class IncomeService() {
 
     fun balance(entries: List<Entry>): Float {
-
-        val values = entries.map {
-            if (it.type.equals(EntryType.INCOME))
-                it.amount
-            else it.amount * -1
-        }
-
-        return values.reduce { acc, it -> acc + it }
+        return entries
+            .map { it.getSignedAmount() }
+            .reduce { acc, it -> acc + it }
     }
 }
