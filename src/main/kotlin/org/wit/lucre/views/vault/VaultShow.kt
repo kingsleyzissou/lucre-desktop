f1d1341bd731f747cc48fa4271a53ad483921daa
Gianluca <gianlucazuccarelli@gmail.com>
2020-11-03 22:58:38 +0000

    chore: refactor vault show view table

--- src/main/kotlin/org/wit/lucre/views/vault/VaultShow.kt
+++ src/main/kotlin/org/wit/lucre/views/vault/VaultShow.kt
@@ -1,17 +1,23 @@
 package org.wit.lucre.views.vault
 
-import org.wit.lucre.controllers.VaultController
 import org.wit.lucre.viewmodels.VaultModel
+import org.wit.lucre.views.entry.EntryIndex
 import tornadofx.* // ktlint-disable no-wildcard-imports
 
 class VaultShow : Fragment("Show Vault") {
     val model: VaultModel by inject()
-    private val vaultController: VaultController by inject()
-    private val data = vaultController.show().asObservable()
+
+    private val entryView = getEntryIndexView()
 
     override val root = borderpane {
         top = label(model.name)
-        center = tableview(data) {
-        }
+        center = entryView.root
+    }
+
+    private fun getEntryIndexView(): EntryIndex {
+        val v = VaultModel(model.item)
+        val scope = Scope()
+        setInScope(v, scope)
+        return find(EntryIndex::class, scope)
     }
 }
