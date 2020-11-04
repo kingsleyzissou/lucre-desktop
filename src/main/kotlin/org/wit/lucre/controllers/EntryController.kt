3270211cba31bed90d3a17aedb242ae3ea63c417
Gianluca <gianlucazuccarelli@gmail.com>
2020-11-04 15:43:34 +0000

    feat: add spending by category logic

--- src/main/kotlin/org/wit/lucre/controllers/EntryController.kt
+++ src/main/kotlin/org/wit/lucre/controllers/EntryController.kt
@@ -2,12 +2,14 @@ package org.wit.lucre.controllers
 
 import org.wit.lucre.models.Entry
 import org.wit.lucre.repositories.EntryStore
+import org.wit.lucre.services.IncomeService
 import tornadofx.Controller
 import java.util.function.Predicate
 
 class EntryController : Controller() {
 
     private val store = EntryStore()
+    private val service = IncomeService()
 
     fun index(): List<Entry> {
         return store.all()
@@ -15,6 +17,10 @@ class EntryController : Controller() {
 
     fun filter(predicate: Predicate<Entry>): List<Entry> {
         return store.where(predicate)
+    }
+
+    fun chartData(entries: List<Entry>): Map<String, Double> {
+        return service.expenseCategories(entries)
     }
 
     fun find(id: String): Entry {
