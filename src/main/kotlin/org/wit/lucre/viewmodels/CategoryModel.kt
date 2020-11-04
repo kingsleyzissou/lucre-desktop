8850cca51d2b17d9e5da33054fec97ff265093ae
Gianluca <gianlucazuccarelli@gmail.com>
2020-11-04 19:13:03 +0000

    fix: category view model constructor params

--- src/main/kotlin/org/wit/lucre/viewmodels/CategoryModel.kt
+++ src/main/kotlin/org/wit/lucre/viewmodels/CategoryModel.kt
@@ -3,7 +3,7 @@ package org.wit.lucre.viewmodels
 import org.wit.lucre.models.Category
 import tornadofx.ItemViewModel
 
-class CategoryModel : ItemViewModel<Category>() {
+class CategoryModel(category: Category) : ItemViewModel<Category>(category) {
     val name = bind(Category::nameProperty)
     val description = bind(Category::descProperty)
     val color = bind(Category::colorProperty)
