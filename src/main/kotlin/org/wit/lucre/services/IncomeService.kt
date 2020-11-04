1fd0a40445fd8d0434f7821b4f216fde8f163525
Gianluca <gianlucazuccarelli@gmail.com>
2020-11-04 19:12:08 +0000

    fix: paramter shadowing issue

--- src/main/kotlin/org/wit/lucre/services/IncomeService.kt
+++ src/main/kotlin/org/wit/lucre/services/IncomeService.kt
@@ -18,7 +18,7 @@ class IncomeService() {
             .groupBy { it.category }
             .mapKeys { it.key.name }
             .mapValues {
-                it.value
+                e -> e.value
                     .map { it.amount.toDouble() }
                     .reduce { acc, it -> acc + it }
             }
