c56bfb903000cd09d008ffb2786dfceb87d94904
Gianluca <gianlucazuccarelli@gmail.com>
2020-11-04 22:33:52 +0000

    fix: chart to show correct color & values

--- src/main/kotlin/org/wit/lucre/views/entry/EntryChart.kt
+++ src/main/kotlin/org/wit/lucre/views/entry/EntryChart.kt
@@ -1,17 +1,23 @@
 package org.wit.lucre.views.entry
 
+import javafx.scene.chart.PieChart
 import org.wit.lucre.controllers.EntryController
 import org.wit.lucre.events.EntriesFilterEvent
 import tornadofx.*
 
-class EntryChart : Fragment("My View") {
+class EntryChart : Fragment("Chart View") {
     private val entryController: EntryController by inject()
 
     override val root = piechart("") {
         maxHeight = 100.0
         subscribe<EntriesFilterEvent> { event ->
-            val d = entryController.chartData((event.entries))
-            data(d)
+            val chartData = entryController.chartData((event.entries))
+            for ((key, value) in chartData) {
+                var d = PieChart.Data(key.name, value)
+                data.add(d)
+                val index = data.indexOf(d)
+                data[index].node.style = "-fx-pie-color: ${key.hexColor};"
+            }
         }
     }
 }
